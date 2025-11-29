6c888f06c92144e8d11f2f5b7215d1aa6279ada8
RaginiVanakhade <raginivankhade1203@gmail.com>
2025-11-29 22:10:23 +0530

    add logout btn

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,11 +5,14 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { IoIosNotifications } from "react-icons/io";
 import { TbHistoryToggle } from "react-icons/tb";
 import ToggleSlide from '../custom/ToggleSlide';
+import { RiLogoutCircleRLine } from "react-icons/ri";
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isSetting, setIsSetting] = useState(false)
   const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+  const navigate = useNavigate();
   return (
     <div className=''>
       <div className="h-full p-2 w-full flex justify-between bg-[#1F1F1F]  shadow-xl relative ">
@@ -21,35 +24,37 @@ const Header = () => {
         <div className=' m-1 p-2 w-full flex justify-between'>
           <div className='flex justify-evenly w-72 font-semibold  '>
             <div className='flex gap-2 cursor-pointer' onClick={() => {
-  setIsSetting(!isSetting);
-  setIsNotificationOpen(false);
-}}>Setting
+              setIsSetting(!isSetting);
+              setIsNotificationOpen(false);
+            }}>Setting
               <div className=''><MdOutlineKeyboardArrowDown size={30} /></div>
             </div>
             <div>
               <div className='flex gap-2 cursor-pointer' onClick={() => {
-  setIsNotificationOpen(!isNotificationOpen);
-  setIsSetting(false);
-}}>Notification
+                setIsNotificationOpen(!isNotificationOpen);
+                setIsSetting(false);
+              }}>Notification
                 <div className=' bg-red-400 rounded-full'><IoIosNotifications size={25} /></div>
               </div>
             </div>
 
             <div>
-          {isNotificationOpen && (
-            <div className="absolute left-96 top-16 z-50">
-              <ToggleSlide  title='Notification'/>
+              {isNotificationOpen && (
+                <div className="absolute left-96 top-16 z-50">
+                  <ToggleSlide title='Notification' />
+                </div>
+              )}
             </div>
-          )}
-        </div>
 
           </div>
-          <div className='flex gap-2 font-semibold cursor-pointer'> <div> <TbHistoryToggle size={25} /></div>Status Updates</div>
+          <div className='flex gap-2 font-semibold cursor-pointer'> <div> <TbHistoryToggle size={25} /></div>Status Updates 
+          <span><button onClick={() => navigate("/")}><RiLogoutCircleRLine  size={25}/></button></span>
+          </div>
         </div>
         <div>
           {isSetting && (
-            <div className="absolute left-96 top-16 z-50">
-              <ToggleSlide  title='Setting'/>
+            <div className="absolute left-80 top-16 z-50 h-[100%] w-[20%]">
+              <ToggleSlide title='Setting' />
             </div>
           )}
         </div>
--- src/custom/ToggleSlide.tsx
+++ src/custom/ToggleSlide.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ToggleSlide = ({
   title= "",
-  width = "200%",
-  height = "150%",
+  width = "100%",
+  height = "500%",
   bgColor = "#f87171",
 }) => {
   return (
